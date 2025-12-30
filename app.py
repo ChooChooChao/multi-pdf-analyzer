8214b2a37752a1f8d1de19a714432f8b7fe23bd8
nchao03 <62034613+ChooChooChao@users.noreply.github.com>
2025-12-30 14:07:37 -0800

    Processing PDFS
    Use PyPDF2 library to read each document and extract the text from it into one large raw_text string that will be split for chunks and used for embedding

--- app.py
+++ app.py
@@ -1,5 +1,14 @@
 import streamlit as st
 from dotenv import load_dotenv
+from PyPDF2 import PdfReader
+
+def get_pdf_text(pdf_docs):
+    text = ""
+    for pdf in pdf_docs: # loop through pdfs
+        pdf_reader = PdfReader(pdf) # creates pages from the pdf
+        for page in pdf_reader.pages: # loop through pages
+            text += page.extract_text() # extract and append the text
+    return text
 
 
 
@@ -14,8 +23,17 @@ def main():
     # sidebar where users input docs
     with st.sidebar: # Note: Keyword "with" used to put things inside sidebar
         st.subheader("Your documents")
-        st.file_uploader("Upload your PDFs here and click on process") # element that allows the actual upload of files
-        st.button("Process")
+        pdf_docs = st.file_uploader("Upload your PDFs here and click on process", accept_multiple_files=True) # element that allows the actual upload of files
+        if st.button("Process"): # make conditional to trigger only when pressed
+            with st.spinner("Processing"): # visual spinning wheel to inform user it is running
+                # get the pdf text
+                raw_text = get_pdf_text(pdf_docs)
+                st.write(raw_text)
+
+                # get the text chunks
+
+                # create vector store
+
 
 if __name__ == "__main__":
     main()
