2d844f640f340b2acba2ff0dd9791a620cb855fe
nchao03 <62034613+ChooChooChao@users.noreply.github.com>
2025-12-30 12:07:50 -0800

    Create GUI
    Following the tutorial video, created a basic GUI using streamlit that allows users to upload documents and then an area where they can ask their questions.

---
+++ app.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+
+def main():
+    st.set_page_config(page_title="Chat with multiple PDFs", page_icon=":books:") # tab/page title
+
+    st.header("Chat with multiple PDFs :books:") # Main header on webpage
+    st.text_input("Ask a Question about your documents: ") # text input area for users (text above input area)
+
+    # sidebar where users input docs
+    with st.sidebar: # Note: Keyword "with" used to put things inside sidebar
+        st.subheader("Your documents")
+        st.file_uploader("Upload your PDFs here and click on process") # element that allows the actual upload of files
+        st.button("Process")
+
+if __name__ == "__main__":
+    main()
